249fb23c2b44c4069e8ecb799e1507ffbd75411e
xelva <xelvacodes@gmail.com>
2022-04-25 09:32:08 -0500

    refactor constructor

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 class App extends React.Component {
-    constructor(props) {
+    /* constructor(props) {
         super(props);
-        
-        //initialize with a property of the obj w state obj. This is the only time we do direct assignment to this.state
         this.state = { lat: null, errorMessage: '' };
+    } */
 
+    state = { lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        console.log('...initial component renedered to the screen')
         window.navigator.geolocation.getCurrentPosition(
-            //add in success callback argument
-            position => {
-                //call setState to update state
-                this.setState({ lat: position.coords.latitude })
-            },
-            //then enter in a failure callback argument
-            (err) => {
-                this.setState( { errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState( { errorMessage: err.message })
         );
+    }
+
+    componentDidUpdate() {
+        console.log('...and it has been updated and rerendered')
     }
     //render is required by react
     render() {
@@ -37,6 +37,7 @@ class App extends React.Component {
         return <div>Loading</div>
        
     }
+
 }
 
 root.render(<App />)
